bdc616ac4be5e9b863ccd18c17d717477fc1e138
Adithyan PB <adithyanpb2005@gmail.com>
2025-11-19 21:48:22 +0530

    backend created, DB and cloudinary api connected, product and user models created

---
+++ models/productModel.js
@@ -0,0 +1,44 @@
+import { MongoOIDCError } from "mongodb";
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: Array,
+        required: true
+    },
+    bestseller: {
+        type: Boolean,
+    },
+    date: {
+        type: Number,
+        required: true
+    }
+})
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongoDB.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// App Config 
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+connectDB()
+connectCloudinary();
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// Api End Points
+
+
+app.get('/',(req, res)=>{
+    res.send("Api working")
+});
+
+app.listen(port, ()=> {
+    console.log(`http://localhost:${port}`);
+    
+});

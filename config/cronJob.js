f354ed9f52531f4ef77a487350b33d4d11b6ab43
Adithyan PB <adithyanpb2005@gmail.com>
2025-12-10 11:55:28 +0530

    cron job added

---
+++ config/cronJob.js
@@ -0,0 +1,14 @@
+import cron from 'cron';
+import https from 'https';
+
+const job = new cron.CronJob("*/14 * * * *", function(){
+    https.get(process.env.API_URL, (res)=> {
+        if(res.statusCode === 200){
+            console.log("cron request sended successfully");
+        }else{
+            console.log("cron request not send");
+        }
+    }).on("error", (e)=> console.error("Error while sending request ", e));
+})
+
+export default job
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import job from './config/cronJob.js'
 
 // App Config 
 
@@ -19,6 +20,15 @@ connectCloudinary();
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+job.start();
+
+// cron job
+
+app.get("/api/health",(req, res) => {
+    console.log("Cron Request Recieved");
+    res.status(200).json({status: "ok"});
+});
 
 // Api End Points
 
